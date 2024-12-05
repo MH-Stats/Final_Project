1038594120ffe9f7019cb6efbb9ee6e36883aa7c
MH-Stats <markheinen7@gmail.com>
2024-12-05 05:10:20 -0500

    Finshed Modeling QMD started API

---
+++ API.R
@@ -0,0 +1,105 @@
+# Loading in plumber package to create API
+library(plumber)
+# loading in other libraries
+library(tidyverse)
+library(tidymodels)
+library(ranger)
+library(parsnip)
+library(rpart.plot)
+
+# Reading in Data
+dhi_data <- read_csv("diabetes_binary_health_indicators_BRFSS2015.csv")
+dhi_data
+
+# Function to change binary variables into factors
+binary_creator <- function(data){
+  # Grabbing all binary cols
+  binary_cols <- colnames(data)[sapply(data, function(col) all(col %in% c(0, 1)))]
+  
+  # removing sex from binary_cols
+  binary_cols <- setdiff(binary_cols, "Sex")
+  
+  # Creating Binary cols
+  for(var in colnames(data)){
+    if(var %in% binary_cols){
+      data[[var]] <- factor(data[[var]], levels = c(0,1), labels = c("No", "Yes"))
+    }
+  }
+  return(data)
+}
+# Running function
+dhi_data <- binary_creator(dhi_data)
+
+# Converting other factor variables 
+dhi_data <- dhi_data |>
+  mutate(
+    Sex = factor(Sex, levels = c(0,1), labels = c("female", "male")),
+    GenHlth = factor(GenHlth, levels = c(1,2,3,4,5), labels = c("excellent", "very_good", "good", "fair", "poor")),
+    Education = factor(Education, levels = c(1,2,3,4,5,6), labels = c("no_schooling", "1_to_8", "9_to_11", "12_or_GED", "some_college", "college_grad")),
+    Income = factor(Income, levels = c(1,2,3,4,5,6,7,8), labels = c("10k_less", "15k_less", "20k_less", "25k_less", "35k_less", "50k_less", "75k_less", "75k_more"))
+  )
+
+# renaming columns 
+dhi_data <- dhi_data |>
+  rename(
+    "Diabetes" = Diabetes_binary,
+    "HeartDisease" = HeartDiseaseorAttack,
+    "HvyAlch" = HvyAlcoholConsump,
+    "Healthcare" = AnyHealthcare,
+    "NoDoc" = NoDocbcCost,
+  )
+
+# Creating the model
+
+# Data split and 5 fold CV (Will be using this later to get the best possible model)
+dhi_split <- initial_split(model_data, prop = 0.7, strata = Diabetes)
+dhi_train <- training(dhi_split)
+dhi_test <- testing(dhi_split)
+dhi_cv <- vfold_cv(dhi_train, 5, strata = Diabetes)
+
+# Recipe for model
+class_recipe <- recipe(Diabetes ~ HighBP + HighChol + BMI + Stroke + PhysActivity + HeartDisease,
+                       data = dhi_train) |>
+  step_dummy(HighBP, HighChol, Stroke, PhysActivity, HeartDisease)
+
+# Setting grid to be used in fits
+class_grid <- grid_regular(cost_complexity(),
+                           tree_depth(),
+                           min_n(),
+                           levels = 5)
+
+# creating model 
+rf_spec <- rand_forest(mtry = 3) |>
+  set_engine("ranger", importance = "impurity") |>
+  set_mode("classification")
+
+# Creating workflow
+rf_wkflw <- workflow() |>
+  add_recipe(class_recipe) |> 
+  add_model(rf_spec)
+
+# fitting data with tune_grid and grid_regular
+rf_fit <- rf_wkflw |>
+  tune_grid(resamples = dhi_cv,
+            grid = 10,
+            metrics = metric_set(mn_log_loss))
+
+# Grabbing metrics for log loss
+rf_fit |>
+  collect_metrics() |>
+  filter(.metric == "mn_log_loss") |>
+  arrange(mean)
+
+# Grabbing best one
+best_rf_param <- rf_fit |>
+  select_best(metric = "mn_log_loss")
+
+# Using finalize_workflow to create final model
+rf_final <- rf_wkflw |>
+  finalize_workflow(best_rf_param) |>
+  last_fit(dhi_split, metrics = metric_set(mn_log_loss))
+
+# fitting the best model 
+best_overall_model <- rf_wkflw |>
+  finalize_workflow(best_rf_param) |>
+  fit(dhi_data)
